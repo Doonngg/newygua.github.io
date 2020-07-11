2f7e1c950b37788cc995acaf1f26208deff14b3a
Doonngg <61999334+Doonngg@users.noreply.github.com>
2020-07-11 17:02:50 +0800

    3

--- js/fn.js
+++ js/fn.js
@@ -1,3 +1,32 @@
 window.addEventListener("load",function(){
-
+	/* let oBg = document.querySelector(".bg");
+	
+	//获取图片盒子的总高度/3
+	let oHight = parseInt(-oBg.offsetHeight / 3);
+	
+	oBg.addEventListener("click",function(){
+		let pos = 0;
+		let myMove = setInterval(move,5);
+		function move(){
+			if(pos == oHight){
+				clearInterval(myMove);
+			}else{
+				pos= pos - 5;
+				oBg.style.top = pos + "px";
+			}
+			
+		}
+	}) */
+	
+	
+	/* document.addEventListener("mousewheel",function(e){
+		e = e || window.event;
+		if (e.wheelDelta > 0){
+			alert("上")
+		}
+		if (e.wheelDelta < 0){
+			alert("下")
+		}
+	}); */
+	
 })
