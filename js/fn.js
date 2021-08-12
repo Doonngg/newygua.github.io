a86da53498ae96ed32bff86e5d38745b8c12dc61
Doonngg <61999334+Doonngg@users.noreply.github.com>
2021-08-12 16:23:41 +0800

    123

--- js/fn.js
+++ js/fn.js
@@ -18,7 +18,15 @@ window.addEventListener("load",function(){
 		}
 	}) */
 	
-	
+	//打开个人中心
+	document.querySelector(".home_page_login").addEventListener("click",function(){
+		opener = null;
+		window.open("https://doonngg.github.io/yguaWeb/","newwindow","height=812,width=375,toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no,left=1200,top=100")   
+	})
+	document.querySelector(".home_page_signin").addEventListener("click",function(){
+		opener = null;
+		window.open("https://doonngg.github.io/yguaWeb/signin.html","newwindow","height=812,width=375,toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no,left=1200,top=100")   
+	})
 	/* document.addEventListener("mousewheel",function(e){
 		e = e || window.event;
 		if (e.wheelDelta > 0){
